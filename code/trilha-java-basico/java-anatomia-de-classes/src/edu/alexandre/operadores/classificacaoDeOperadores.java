2203af710a9f78dad1894d874e0f5bb118bc6ae8
alexandre <alexandre.matheus.oliveira@gmail.com>
2023-08-30 16:56:57 -0300

    aprendendo sobre operadores

---
+++ code/trilha-java-basico/java-anatomia-de-classes/src/edu/alexandre/operadores/classificacaoDeOperadores.java
@@ -0,0 +1,52 @@
+package edu.alexandre.operadores;
+
+public class classificacaoDeOperadores {
+    public static void main(String[] args) {
+    
+    /*//classe Operadores de Atribuição
+    String nome = "GLEYSON";
+    int idade = 22;
+    double peso = 68.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    //Date dataNascimento = new Date();
+
+
+    //classe Operadores Aritméticos
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    //int modulo = 18 % 3;
+    //double resultado = (10 * 7) + (20/4);
+
+    //classe Operadores Unários
+    //int numero = 5;
+            
+    //Imprimindo o numero negativo
+    //System.out.println(- numero);
+
+    //incrementando numero em mais 1 numero, imprime 6
+    //numero ++;
+    //System.out.println(numero);
+
+    //incrementando numero em mais 1 numero, imprime 7
+    //System.out.println(numero ++);// ops algo de errado não está certo
+
+    //System.out.println(numero);// agora sim, numero virou 7
+
+    //ordem de precedencia conta aqui
+    //System.out.println(++ numero);
+
+    boolean verdadeiro = true;
+
+    System.out.println("Inverteu " + !verdadeiro);
+
+    //classe Operadores Ternário
+
+    //classe Operadores Relacionais
+
+    //classe Operadores Lógicos */
+    
+    }
+}
