a5240426ad85f64ae2b4cb7da31c012ebeba5ab5
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2023-09-01 08:45:17 -0300

    aprendendo sobre documentação

--- code/trilha-java-basico/java-anatomia-de-classes/src/edu/alexandre/comentarios/Comentarios.java
+++ code/trilha-java-basico/java-anatomia-de-classes/src/edu/alexandre/comentarios/Comentarios.java
@@ -1,4 +1,4 @@
-package edu.alexandre
+package edu.alexandre.comentarios
 public class Comentarios{
     public static void main(String[] args) {
         // Olá, eu sou um comentário em uma única linha
---
+++ code/trilha-java-basico/java-anatomia-de-classes/src/edu/alexandre/documentacao/Calculadora.java
@@ -0,0 +1,6 @@
+package edu.alexandre.documentacao
+public class Calculadora{
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois
+    }
+}
