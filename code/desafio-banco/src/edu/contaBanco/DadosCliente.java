2108bf2575640f61ecb45e8707f03e7108dbcc5c
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2023-09-03 17:16:52 +0000

    alterando parâmetros da conta

--- code/desafio-banco/src/edu/contaBanco/DadosCliente.java
+++ code/desafio-banco/src/edu/contaBanco/DadosCliente.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class DadosCliente{
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
     String nomeCliente;
-    
+    String agencia; 
+    int numeroConta;
+    double saldo;
+
     public void criarConta(){
         System.out.println("Digite o seu nome completo: ");
         nomeCliente = scanner.next();
-        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque");
+        System.out.println("Insira a Agência: ");
+        agencia = scanner.next();
+        System.out.println("Insira o número da conta: ");
+        numeroConta = scanner.nextInt();
+        System.out.println("Insira o saldo inicial: ");
+        saldo = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo +" já está disponível para saque");
     }
 }
