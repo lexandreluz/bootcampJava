208281255dd6bebb3d70e8d5f36f034d4ce11cb5
Alexandre Luz <43273863+lexandreluz@users.noreply.github.com>
2023-10-17 14:00:01 +0000

    aprendendo sobre poo

--- code/poo/pilares/src/MSNMessenger.java
+++ code/poo/pilares/src/MSNMessenger.java
@@ -1,21 +1,8 @@
-public class MSNMessenger {
-	public void enviarMensagem() {
-		//primeiro confirmar se esta conectado a internet
-		validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-		
-		//depois de enviada, salva o histórico da mensagem
-		salvarHistoricoMensagem();
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
-	
-	//métodos privadas, visíveis somente na classe
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
+public class MSNMessenger extends ServicoMensagemInstantanea{
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo MSN Messenger");
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN Messenger");
+    }
 }
